e474fc462220bfbd3de4a1f4823939bb51a7858b
Aitor Jiménez Yañez <aitor@ajimenez.es>
2019-10-20 17:53:44 +0200

    Use a folder inside documents instead of global documents

--- WargameTournamentManager/CreateTournamentScreen.xaml.cs
+++ WargameTournamentManager/CreateTournamentScreen.xaml.cs
@@ -2,21 +2,10 @@
 using Microsoft.Win32;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace WargameTournamentManager
 {
@@ -58,9 +47,13 @@ namespace WargameTournamentManager
                 return;
             }
             SaveFileDialog savefile = new SaveFileDialog();
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "WargameTournamentManager");
+            Directory.CreateDirectory(folder);
+
             // Tournament file
             savefile.FileName = "tournament" + creationTournament.Name + ".tour";
             savefile.Filter = "Tournament files (*.tour)|*.tour|All files (*.*)|*.*";
+            savefile.InitialDirectory = folder;
 
             bool? saved = savefile.ShowDialog();
             if (saved == true)
