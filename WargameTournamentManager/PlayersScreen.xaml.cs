88e2440dcb568c2c4d35361f28522ee45ed126cf
Aitor Jiménez Yañez <aitor@ajimenez.es>
2019-11-22 19:22:36 +0100

    Show how many players have been added

--- WargameTournamentManager/PlayersScreen.xaml.cs
+++ WargameTournamentManager/PlayersScreen.xaml.cs
@@ -205,4 +205,17 @@ namespace WargameTournamentManager
             throw new NotSupportedException();
         }
     }
+
+    public class PlayerListCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("Hay {0} jugadores registrados", MainWindow.gMainWindow.currentTournament.Players.Count);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
